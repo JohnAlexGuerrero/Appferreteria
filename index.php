e14e897be145f7cd7435acb431be3143b5563ec7
jago <johnalexguerrero@gmail.com>
2019-06-20 17:41:53 -0500

    home

--- index.php
+++ index.php
@@ -4,9 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Ferreteria</title>
+
 </head>
-<body>
-    
+<body class="fondo-body">
+
+<div id="sidebar" class="active">
+    <div class="toggle-btn">
+        <span>&#9776;</span>
+    </div>
+    <ul>
+        <li>home</li>
+        <li>home</li>
+        <li>home</li>
+        <li>home</li>
+    </ul>
+</div>
+
+<div id="contenedor" class="container-panel">
+    <span>ldfldfkldkf</span>
+</div>
+  
+
+<script src="main.js" charset="utf-8"></script>
+
 </body>
 </html>
