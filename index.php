6114694e281c7df440453c081f3574d5fe1c7812
johnalexguerrero <johnalexguerrero@gmail.com>
2019-06-20 23:57:13 -0500

    home update

--- index.php
+++ index.php
@@ -11,24 +11,44 @@
     <title>Ferreteria</title>
 
 </head>
-<body class="fondo-body">
 
-<div id="sidebar" class="active">
-    <div class="toggle-btn">
-        <span>&#9776;</span>
-    </div>
-    <ul>
-        <li>home</li>
-        <li>home</li>
-        <li>home</li>
-        <li>home</li>
+<body class="bg-light">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <a class="navbar-brand" href="index.php"><strong>Soluciones Ferreteras</strong> </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor03">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
     </ul>
-</div>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
 
-<div id="contenedor" class="container-panel">
-    <span>ldfldfkldkf</span>
+</nav>
+
+<div class="container my-1">
+<div class="alert alert-dismissible alert-light">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>Heads up!</strong> This <a href="#" class="alert-link">alert needs your attention</a>, but it's not super important.
 </div>
-  
+</div>
 
 <script src="main.js" charset="utf-8"></script>
 
